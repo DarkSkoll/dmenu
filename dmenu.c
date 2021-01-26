a9b523d2cab7e09e1ee65bc827f3c2f10dd0fc19
darkskoll <erick.lievana@gmail.com>
2021-01-26 14:53:52 -0600

    Fix for border patch
    By default the border patch adds the border size to the width of the
    window. With this commit the width will subtract the border size, to
    give the real width.

--- dmenu.c
+++ dmenu.c
@@ -674,7 +674,7 @@ setup(void)
 	swa.background_pixel = scheme[SchemeNorm][ColBg].pixel;
 	swa.event_mask = ExposureMask | KeyPressMask | VisibilityChangeMask;
 	//win = XCreateWindow(dpy, parentwin, x, y, mw, mh, 0,
-    win = XCreateWindow(dpy, parentwin, x, y, mw, mh, border_width,
+    win = XCreateWindow(dpy, parentwin, x, y, mw-(border_width*2), mh, border_width,
 	                    CopyFromParent, CopyFromParent, CopyFromParent,
 	                    CWOverrideRedirect | CWBackPixel | CWEventMask, &swa);
     XSetWindowBorder(dpy, win, scheme[SchemeSel][ColBg].pixel);
